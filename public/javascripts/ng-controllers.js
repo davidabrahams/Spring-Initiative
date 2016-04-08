51496f1845090906dc7ed1a5e0871fe482821c49
Annabel <annabel.consilvio@gmail.com>
2016-04-08 16:55:10 -0400

    INITIAL d3 commit, almost working bar chart

--- public/javascripts/ng-controllers.js
+++ public/javascripts/ng-controllers.js
@@ -85,6 +85,13 @@ springInitiative.controller('programController', function($scope, $rootScope, $h
 
 springInitiative.controller('studentController', function($scope,  $rootScope, $http) {
 
+  $scope.options = {width: 500, height: 300, 'bar': 'aaa'};
+  $scope.data = $rootScope.currentStudent.attendence;
+  $scope.hovered = function(d){
+      $scope.barValue = d;
+      $scope.$apply();
+    };
+  $scope.barValue = 'None';
   //TODO: check these and make sure they work
   $scope.submitEditStudent = function(editStudent, currentStudent) {
     $http.post('api/student/edit/' + currentStudent._id, editStudent)
@@ -99,6 +106,7 @@ springInitiative.controller('studentController', function($scope,  $rootScope, $
   }
 
   $scope.submitNewEntry = function(student) {
+
     $http.post('api/student/newEntry/' + student._id, $scope.newEntry)
       .success(function(data) {
         $scope.$parent.students = data.allStudents;
@@ -124,7 +132,49 @@ springInitiative.controller('studentController', function($scope,  $rootScope, $
       });
   }
 
-});
+}).directive('barChart', function(){
+    var chart = d3.custom.barChart();
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div class="chart"></div>',
+        scope:{
+            height: '=height',
+            data: '=data',
+            hovered: '&hovered'
+        },
+        link: function(scope, element, attrs) {
+            var chartEl = d3.select(element[0]);
+            chart.on('customHover', function(d, i){
+                scope.hovered({args:d});
+            });
+
+            scope.$watch('data', function (newVal, oldVal) {
+                chartEl.datum(newVal).call(chart);
+            });
+
+            scope.$watch('height', function(d, i){
+                chartEl.call(chart.height(scope.height));
+            })
+        }
+    }
+  })
+  .directive('chartForm', function(){
+    return {
+        restrict: 'E',
+        replace: true,
+        controller: function AppCtrl ($scope) {
+            $scope.update = function(d, i){ $scope.data = randomData(); };
+            function randomData(){
+                return d3.range(~~(Math.random()*50)+1).map(function(d, i){return ~~(Math.random()*1000);});
+            }
+        },
+        template: '<div class="form">' +
+                'Height: {{options.height}}<br />' +
+                '<input type="range" ng-model="options.height" min="100" max="800"/>' +
+                '<br /><button ng-click="update()">Update Data</button>' +
+                '<br />Hovered bar data: {{barValue}}</div>'
+    }});
 
 springInitiative.controller('addStudentController', function($scope, $rootScope, $http, $location){
   $scope.addStudent = function() {
