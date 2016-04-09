1132fdc9049794722af969b6abb5da44826a8a43
David Abrahams <david.abrahams@students.olin.edu>
2016-04-08 21:00:56 -0400

    MOVE: moved student view to own file

--- public/javascripts/ng-apps.js
+++ public/javascripts/ng-apps.js
@@ -41,6 +41,16 @@ springInitiative.config(function($stateProvider, $urlRouterProvider, $locationPr
         'content': {
           templateUrl: 'views/content/student.html',
           controller: 'studentController'
+        },
+        'studentView@index.student': {
+          templateUrl: 'views/content/studentView.html'
+        }
+      }
+    })
+    .state('index.student.addEntry', {
+      views: {
+        'studentView@index.student': {
+          templateUrl: 'views/content/entryForm.html'
         }
       }
     })
