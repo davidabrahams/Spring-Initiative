f9714a246e66bf63f42cb834ce81714ad571fabc
Annabel <annabel.consilvio@gmail.com>
2016-04-12 16:29:37 -0400

    FIX: .success to .then

--- public/javascripts/ng-controllers.js
+++ public/javascripts/ng-controllers.js
@@ -80,15 +80,14 @@ springInitiative.controller('programController', function($scope, $http, $locati
 });
 
 springInitiative.controller('d3Controller', function($scope, $http, $state){
-    $http.get('/api/student/data/'+ $scope.parent.currentStudent._id)
-      .success(function(data){
-        $scope.attendance = data.attendanceList;
-        $scope.dates = data.datesList;
-        $scope.stars = data.starsList;
-
-        console.log($scope.attendance, $scope.attendance.sort())
+    $http.get('/api/student/data/'+ $scope.currentStudent._id).then(function successCallback(response){
+        $scope.attendance = response.data.attendanceList;
+        $scope.dates = response.data.datesList;
+        $scope.stars = response.data.starsList;
 
         var count = {}; 
+        //creating a dictionary mapping elements to their counts in a list
+        //http://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
         $scope.attendance.forEach(function(i) { count[i] = (count[i]||0)+1;  });
 
         var dataList = [];
@@ -123,10 +122,9 @@ springInitiative.controller('d3Controller', function($scope, $http, $state){
           };
 
         $scope.data = dataList;
-      })
-      .error(function(data) {
-          console.log('Error: ' + data);
-      });
+    }, function errorCallback(response){
+        console.log('Error: ' + response.data);
+    })
 });
 
 springInitiative.controller('studentController', function($scope,  $http, $state) {
