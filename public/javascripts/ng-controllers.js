338333bf23cf6e19e61f322c527f0b1b2167ae51
David Abrahams <david.abrahams@students.olin.edu>
2016-04-12 16:36:09 -0400

    EDIT: making code pretty

--- public/javascripts/ng-controllers.js
+++ public/javascripts/ng-controllers.js
@@ -74,57 +74,48 @@ springInitiative.controller('indexController', function($scope, $http,
 
 springInitiative.controller('overviewController', function($scope, $http, $location) {});
 
-springInitiative.controller('programController', function($scope, $http, $location) {
-  $scope.programName = 'something spring';
-  $scope.programInfo = ':3';
-});
+springInitiative.controller('d3Controller', function($scope, $http, $state) {
+  $http.get('/api/student/data/' + $scope.currentStudent._id).then(function successCallback(
+    response) {
+    $scope.attendance = response.data.attendanceList;
+    $scope.dates = response.data.datesList;
+    $scope.stars = response.data.starsList;
 
-springInitiative.controller('d3Controller', function($scope, $http, $state){
-    $http.get('/api/student/data/'+ $scope.currentStudent._id).then(function successCallback(response){
-        $scope.attendance = response.data.attendanceList;
-        $scope.dates = response.data.datesList;
-        $scope.stars = response.data.starsList;
+    var count = {};
+    //creating a dictionary mapping elements to their counts in a list
+    //http://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
+    $scope.attendance.forEach(function(i) {
+      count[i] = (count[i] || 0) + 1;
+    });
 
-        var count = {}; 
-        //creating a dictionary mapping elements to their counts in a list
-        //http://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
-        $scope.attendance.forEach(function(i) { count[i] = (count[i]||0)+1;  });
+    var dataList = [];
+    var keys = Object.keys(count);
 
-        var dataList = [];
-        var keys = Object.keys(count);
-        var idea = "Excused"
-        console.log(count[idea])
+    for (var i = 0; i < keys.length; i++) {
+      console.log(count)
+      dataList.push({
+        key: keys[i],
+        y: count[keys[i]]
+      })
+    }
+    $scope.options = {
+      chart: {
+        type: 'pieChart',
+        height: 500,
+        x: function(d) { return d.key; },
+        y: function(d) { return d.y; },
+        showLabels: true,
+        duration: 500,
+        labelThreshold: 0.01,
+        labelSunbeamLayout: true,
+        legend: { margin: { top: 5, right: 35, bottom: 5, left: 0 } }
+      }
+    };
 
-        for(var i = 0; i < keys.length; i++){
-          console.log(count)
-          dataList.push({key:keys[i], y:count[keys[i]]})
-        }
-        console.log(dataList)
-         $scope.options = {
-            chart: {
-              type: 'pieChart',
-              height: 500,
-              x: function(d){return d.key;},
-              y: function(d){return d.y;},
-              showLabels: true,
-              duration: 500,
-              labelThreshold: 0.01,
-              labelSunbeamLayout: true,
-              legend: {
-                margin: {
-                  top: 5,
-                  right: 35,
-                  bottom: 5,
-                  left: 0
-                }
-              }
-            }
-          };
-
-        $scope.data = dataList;
-    }, function errorCallback(response){
-        console.log('Error: ' + response.data);
-    })
+    $scope.data = dataList;
+  }, function errorCallback(response) {
+    console.log('Error: ' + response.data);
+  })
 });
 
 springInitiative.controller('studentController', function($scope,  $http, $state) {
