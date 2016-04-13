fd7a9cbf42b53e9c6892ab97a37a22f9d90bbbdf
David Abrahams <david.abrahams@students.olin.edu>
2016-04-13 19:49:20 -0400

    MOVE: index controller

--- public/javascripts/ng-controllers.js
+++ public/javascripts/ng-controllers.js
@@ -1,36 +1,5 @@
 springInitiative.controller('loginController', loginController);
-
-
-springInitiative.controller('indexController', function($scope, $http,
-                            $location, $state) {
-
-  $scope.students = [];
-
-  $http.get('/user').then(function(data) {
-    $scope.user = data.data.user
-    console.log("Current user: " + $scope.user.email);
-
-  })
-
-  $scope.logout = function(){
-    $http.post("/api/logout").then(function successCallback(response) {
-      $state.go('login');
-    }, function errorCallback(response) {
-      console.log("Error: " + data);
-    });
-  }
-
-  $http.get('/api/allStudents').then(function(data) {
-    $scope.students = data.data;
-  }, function(err) {
-    console.log('Error: in GET \'/student\'', err);
-  });
-
-  $scope.showStudent = function(student){
-    $scope.currentStudent = student;
-    $scope.editStudent = angular.copy(student);
-  }
-});
+springInitiative.controller('indexController', indexController);
 
 springInitiative.controller('d3Controller', function($scope, $http, $state) {
   $http.get('/api/student/data/' + $scope.currentStudent._id).then(function successCallback(
