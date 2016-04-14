3ac2042f6093ae4ea8b69910a48f2c4f7da35a70
David Abrahams <david.abrahams@students.olin.edu>
2016-04-13 20:27:46 -0400

    moved student controllers

--- public/javascripts/ngControllers.js
+++ public/javascripts/ngControllers.js
@@ -3,61 +3,11 @@ springInitiative.controller('indexController', indexController);
 
 // springInitiative.controller('d3Controller', d3Controller);
 
-springInitiative.controller('studentController', function($scope,  $http, $state) {
+springInitiative.controller('studentController', studentController);
 
-  $scope.$state = $state;
-  //TODO: check these and make sure they work
-  $scope.submitEditStudent = function(editStudent, currentStudent) {
-    $http.post('api/student/edit/' + currentStudent._id, editStudent)
-    .then(function successCallback(response) {
-      $scope.$parent.students = response.data.allStudents;
-      $scope.$parent.currentStudent = response.data.currentStudent;
-      $scope.studentEditMsg = response.data.msg;
-    }, function errorCallback(response) {
-        console.log('Error: ' + response.data);
-        $scope.studentEditMsg = response.data.msg;
-    });
-  }
+springInitiative.controller('addEntryController', addEntryController);
 
-  // TODO: .success and .error are deprecated
-  $scope.getArchive = function() {
-    $http.get('api/index/archive').then(function successCallback(response) {
-      $scope.allArchivedStudents = response.data;
-    }, function errorCallback(response) {
-      console.log('Error:' + response.data);
-    });
-  }
-
-});
-
-springInitiative.controller('addEntryController', function($scope, $http, $location) {
-  $scope.submitNewEntry = function(student) {
-    $http.post('api/student/newEntry/' + student._id, $scope.newEntry)
-    .then(function successCallback(response) {
-      $scope.entrySubmittedMsg = response.data.msg;
-    }, function errorCallback(response) {
-      console.log('Error: ' + response.data);
-      $scope.entrySubmittedMsg = response.data.msg;
-
-    });
-  }
-});
-
-springInitiative.controller('addStudentController', function($scope, $http,
-  $location) {
-  $scope.addStudent = function() {
-    $http.post('api/student/add', $scope.newStudent).then(function successCallback(
-      response) {
-      $scope.$parent.students = response.data.allStudents;
-      $scope.newStudent = response.data.newStudent;
-      $scope.studentAddedMsg = response.data.msg;
-    }, function errorCallback(response) {
-      console.log('Error: ' + response.data);
-      $scope.studentAddedMsg = response.data.msg;
-    });
-
-  };
-});
+springInitiative.controller('addStudentController', addStudentController);
 
 springInitiative.controller('settingsController', function($scope, $http, $location){
 
