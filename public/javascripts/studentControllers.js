7ea4f950bb7f78e262e310f37013b9d1631b319b
David Abrahams <david.abrahams@students.olin.edu>
2016-05-02 11:54:59 -0400

    ADD: delete functionality for users to delete students

--- public/javascripts/studentControllers.js
+++ public/javascripts/studentControllers.js
@@ -2,7 +2,20 @@ var studentController = function($scope, $http, $state) {
 
   // $scope.$state = $state;
 
-  //TODO: check these and make sure they work
+  $scope.deleteStudent = function(student) {
+    var r = window.confirm("Are you sure you would like to delete this student? This is not the same as archiving, and cannot be undone.");
+    if (r == true) {
+      $http.delete('api/student/' + student._id)
+      .then(function successCallback(response) {
+        $scope.$parent.students = response.data;
+        $state.go('index');
+      }, function errorCallback(response) {
+          console.log('Error: ' + response.data);
+          $scope.studentEditMsg = response.data.msg;
+      });
+    }
+  };
+
   $scope.submitEditStudent = function(editStudent, currentStudent) {
     $http.post('api/student/edit/' + currentStudent._id, editStudent)
     .then(function successCallback(response) {
